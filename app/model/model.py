f7bf58a4eb6802a0033a3f837f0e20df726817d8
DinuthDheeraka <dinuthdheeraka345@gmail.com>
2024-01-20 00:47:43 +0530

    added artist model

--- app/model/model.py
+++ app/model/model.py
@@ -1,12 +1,13 @@
 import sqlalchemy
 from sqlalchemy import Column, Integer, String, Date, DateTime, func
+from sqlalchemy.orm import relationship
 
 Base = sqlalchemy.orm.declarative_base()
 
 
 class Buyer(Base):
     __tablename__ = "buyer"
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     email = Column(String, nullable=False, unique=True)
     password = Column(String, nullable=False)
     full_name = Column(String, nullable=False)
@@ -17,6 +18,7 @@ class Buyer(Base):
     phone_number = Column(String, nullable=False)
     date_of_birth = Column(Date, nullable=False)
     gender = Column(String, default="NONE")
+    profile_img_url = Column(String, default=None)
     buyer_status = Column(String, default="ACTIVE")
     created_at = Column(DateTime, nullable=False, server_default=func.utcnow())
     updated_at = Column(DateTime, nullable=False, server_default=func.utcnow())
@@ -24,7 +26,7 @@ class Buyer(Base):
 
 class Artist(Base):
     __tablename__ = "artist"
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     email = Column(String, nullable=False, unique=True)
     password = Column(String, nullable=False)
     full_name = Column(String, nullable=False)
@@ -35,6 +37,22 @@ class Artist(Base):
     phone_number = Column(String, nullable=False)
     date_of_birth = Column(Date, nullable=False)
     gender = Column(String, default="NONE")
-    buyer_status = Column(String, default="ACTIVE")
+    profile_img_url = Column(String, default=None)
+    profile_background_img_url = Column(String, default=None)
+    biography = Column(String, default=None)
+    artist_category_id = Column(Integer, ForeignKey="artist_category.id")
+    artist_status = Column(String, default="ACTIVE")
+    created_at = Column(DateTime, nullable=False, server_default=func.utcnow())
+    updated_at = Column(DateTime, nullable=False, server_default=func.utcnow())
+
+    # Relationship to the ArtistCategory
+    artist_category = relationship("ArtistCategory", backref="artist")
+
+
+class ArtistCategory(Base):
+    __tablename__ = "artist_category"
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    artist_category_status = Column(String, default="ACTIVE")
     created_at = Column(DateTime, nullable=False, server_default=func.utcnow())
     updated_at = Column(DateTime, nullable=False, server_default=func.utcnow())
